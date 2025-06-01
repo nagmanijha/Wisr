12157876b5677f6937cd5a16dc243d1ad5e56457
Nagmani Jha <nagmanijha31@gmail.com>
2025-06-01 17:06:23 +0530

    Implement Auth, Billing, Database Setup,

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
   subsets: ["latin"],
@@ -18,10 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
+        <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
+          <Navbar />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,54 @@
 import React from 'react'
 import {Button} from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import Cta from '@/components/CTA'
+import {getSubjectColor} from "@/lib/utils";
+import { recentSessions } from '@/constants'
+
 const Page = () => {
   return (
-    <div >
-<h1> Page Title</h1>  
-<Button>
-Lets get
-</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+    <section className='home-section'>
+      <CompanionCard 
+      id="123"
+      name="Neura the Brain Explorer"
+      topic="neuroscience"
+      subject="science"
+      duration={45}
+      color="#ff0000"
+      bookmarked={false}
+      />
+       <CompanionCard 
+      id="456"
+      name="Neura the Brain Explorer"
+      topic="neuroscience"
+      subject="science"
+      duration={45}
+      color="#fff"
+      bookmarked={false}  
+      />
+     <CompanionCard 
+      id="789"
+      name="Neura the Brain Explorer"
+      topic="neuroscience"
+      subject="science"
+      duration={45}
+      color="#11aaaa"
+      bookmarked={false}
+      />
+    </section>
+
+    <section className='home-section'>
+      <CompanionList
+        title="Recent Sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+      />
+        <Cta/>
+    </section>
+    </main>
   )
 }
 
